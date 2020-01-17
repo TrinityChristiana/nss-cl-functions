cb8198e3533384a4710706e84af34fac05577824
Trinity Terry <trinitycterry@gmail.com>
2020-01-17 09:10:28 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+
+let dogName = "Ada Grace";
+let ownerName = "Kristen";
+
+console.log(`${dogName} is owned by ${ownerName}`);
+
+dogname = "Raleigh";
+ownerName = "Margaret";
+
+console.log(`${dogName} is owned by ${ownerName}`);
