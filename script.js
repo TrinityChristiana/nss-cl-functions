fd5eb8ddaf1d012674d05e3e6cdab6f59b77a18a
Trinity Terry <trinitycterry@gmail.com>
2020-01-17 09:23:46 -0600

    created functions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("Hello");
-
+/* 
 let dogName = "Ada Grace";
 let ownerName = "Kristen";
 
@@ -8,4 +8,18 @@ console.log(`${dogName} is owned by ${ownerName}`);
 dogname = "Raleigh";
 ownerName = "Margaret";
 
-console.log(`${dogName} is owned by ${ownerName}`);
+console.log(`${dogName} is owned by ${ownerName}`); */
+
+// Pure functions do not affect any other part of the applications
+let dogOwners = (dogName, ownerName) => {
+    console.log(`${dogName} is owned by ${ownerName}`);
+};
+
+dogOwners("Ada Grace", "Kristen");
+dogOwners("Raleigh", "Margaret");
+dogOwners("Francis", "Jane");
+
+// Impure functions produce a different output with iffferent inputs, or affects another part of the function
+let random = (num) => {
+    Math.random() * 
+}
